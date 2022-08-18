3ce13f18309ad4cebb655d725fdf7757d3ed8c29
grigorii <grigoriiborodin@yandex.ru>
2022-08-18 22:18:09 +0300

    initial commit

---
+++ src/main/java/com/cashline/costupdate/CostUpdateApplication.java
@@ -0,0 +1,13 @@
+package com.cashline.costupdate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CostUpdateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CostUpdateApplication.class, args);
+    }
+
+}
